6b4145892969494c615bb9c1d3cbc404d4ca3616
Aaron Yoon <awesomeyooner@gmail.com>
2026-03-30 21:57:54 -0700

    Included more documentation for the USBFS API

--- include/EmbeddedLib/stm32/communication/usbfs_api.hpp
+++ include/EmbeddedLib/stm32/communication/usbfs_api.hpp
@@ -11,6 +11,21 @@
 extern "C" {
 #endif
 
+    /**
+     * @brief C++ Entrypoint for the USBFS receive callback
+     * 
+     * Include this header in the USB CDC HAL code
+     * 
+     * In File: `cube/USB_DEVICE/App/usbd_cdc_if.h`
+     * 
+     * Add: `#include "EmbeddedLib/stm32/communication/usbfs_api.hpp"` at line 34
+     *
+     * Add the actual API Call
+     * 
+     * In File: `cube/USB_DEVICE/App/usbd_cdc_if.c`
+     * 
+     * Add: `USBFS_on_receive(uint8_t* buffer, uint32_t length)`
+     */
     bool USBFS_on_receive(uint8_t* buffer, uint32_t length);
 
 #ifdef __cplusplus
